b217f4d4f59b53a6ba9c7a9a8089e4fa08e677bb
[Arin Ramer] <[arinramer@gmail.com]>
2019-09-18 14:12:40 -0700

    Done

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,23 +16,42 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
-/*
-  === CharacterStats ===
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
+  function GameObject(foobar) {
+    this.createdAt = foobar.createdAt;
+    this.name = foobar.name;
+    this.dimensions = foobar.dimensions;
+  }
 
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
- 
+  GameObject.prototype.destroy = function(){
+    return this.name + " was removed from the game.";
+  }
+
+
+  function CharacterStats(item){
+    GameObject.call(this, item);
+    this.healthPoints = item.healthPoints; 
+    // prototype method -> returns the string '<object name> took damage.'
+    //should inherit destroy() from GameObject's prototype
+  }
+  CharacterStats.prototype = Object.create(GameObject.prototype);
+  CharacterStats.prototype.takeDamage = function(){
+    return this.name + " took damage.";
+  }
+
+  function Humanoid(thing) {
+    CharacterStats.call(this, thing);
+    this.team = thing.team;
+    this.weapons = thing.weapons;
+    this.language = thing.language;
+    // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+    //should inherit destroy() from GameObject through CharacterStats
+    //should inherit takeDamage() from CharacterStats
+  }
+  Humanoid.prototype = Object.create(CharacterStats.prototype);
+  Humanoid.prototype.greet = function(){
+    return this.name + ' offers a greeting in ' + this.language;
+  }
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +60,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +121,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,52 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding: the this keyword binds to the window object
+* 2. Implicit binding: when a function is invoked it looks to the left of the dot.
+* 3. New binding: uses the 'new' keyword to create a new object
+* 4. Explicit binding: tells javascript to point to a certain value using call, apply or bind.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+function myFunction(){
+  console.log(this.test);
+}
+myFunction();
+const test = "Hello world";
 
 // Principle 2
 
-// code example for Implicit Binding
+let myDog = {
+  name: 'Princess',
+  bark: 'Woof',
+  dog: function(){
+    console.log(this.bark);
+  }
+}
+
+myDog.dog();
 
 // Principle 3
 
-// code example for New Binding
+function cat(meowing){
+    this.thing = meowing;
+}
+
+let myCat = new cat('Meow');
+console.log(myCat.thing);
 
 // Principle 4
 
-// code example for Explicit Binding
+function cow(){
+  console.log(this.moo);
+}
+
+let myCow ={
+  name: 'Dinner',
+  moo: 'mooooo!'
+}
+
+cow.call(myCow);
